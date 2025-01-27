33cc2e1d99057feae0cdb37d1f2beb474f07cb83
Thavasumoorthi <thavasumoorthi@gmail.com>
2025-01-27 11:24:01 +0530

    added secrets env file

--- dangerfile.js
+++ dangerfile.js
@@ -4,4 +4,18 @@ eslintchecking()
 packagejsonchanging()
 
 
+//rule 3 check secrets file are changed:
+
+
+
+const modifiedFiles=danger.git.modified_files
+
+modifiedFiles.forEach((file=>{
+    if(file=="secrets.env")
+    {
+        fail("Secrets env file are changed please monitor the secrets env file")
+    }
+}))
+
+
 
--- sub.js
+++ sub.js
@@ -1,3 +1,5 @@
-const a=10
-const b=20
-const c=20
+const a=10;
+const b=20;
+const c=20;
+
+console.log("Hello Thavasumoorthi");
